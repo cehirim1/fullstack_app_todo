d71136cbcd9e93851f857f4ee716c5e77775ef79
Joy Shaheb <khondokoralam@gmail.com>
2024-01-04 11:19:58 +0600

    backend api completed

--- backend/Controller/TaskController.js
+++ backend/Controller/TaskController.js
@@ -1,11 +1,13 @@
-import { getDB } from "../db.js";
 import { TaskModel } from "../Model/TaskModel.js";
 import { UserModel } from "../Model/UserModel.js";
+
+// Create Task
+
 export const createTask = async (req, res) => {
   try {
     const { task, deadline, status, description, userOwner } = req.body;
     const findUserOwner = await UserModel.findById(userOwner);
-    if (!findUserOwner){
+    if (!findUserOwner) {
       return res.status(404).json({ error: "user not found" });
     }
 
@@ -13,14 +15,63 @@ export const createTask = async (req, res) => {
       task,
       deadline,
       status,
-       description,
-       userOwner,
+      description,
+      userOwner,
     });
-    return res.status(201).json({message: "New Task Created", createTask});
+    return res.status(201).json({ message: "New Task Created", createTask });
   } catch (error) {
     res.status(500).json({ error: "request not sent" });
   }
 };
+
+// GET ALL TASKS
+
 export const getData = async (req, res) => {
-  res.json({ message: "hello world!" });
+  const { userId } = req.params;
+  try {
+    const findUser = await UserModel.findById(userId);
+    if (!findUser) {
+      return res.status(404).json({ error: "user not found" });
+    }
+
+    const findTask = await TaskModel.find({ userOwner: userId });
+
+    res.status(200).json({ message: "hello world!", findTask });
+  } catch (err) {
+    res.status(500).json({ error: "request not sent" });
+  }
+};
+
+// DELETE ONE TASK
+export const deleteTask = async (req, res) => {
+  const { id } = req.params;
+  try {
+    await TaskModel.findByIdAndDelete(id);
+    res.status(200).json({ message: "Task deleted", id });
+  } catch (err) {
+    res.status(500).json({ error: "request not sent" });
+  }
+};
+// UPDATE ONE TASK
+
+export const updateTask = async (req, res) => {
+  const { id } = req.params;
+  const { task, deadline, status, description } = req.body;
+
+  try {
+    await TaskModel.findOneAndUpdate(
+      {
+        _id: id,
+      },
+      {
+        task,
+        deadline,
+        status,
+        description,
+      }
+    );
+    res.status(200).json({ message: "Task updated", id });
+  } catch (err) {
+    res.status(500).json({ error: "request not sent" });
+  }
 };
--- backend/main.js
+++ backend/main.js
@@ -1,6 +1,10 @@
 import express from "express";
 import { createUser } from "./Controller/AuthController.js";
-import { createTask } from "./Controller/TaskController.js";
+import {
+  createTask,
+  deleteTask,
+  updateTask,
+} from "./Controller/TaskController.js";
 import { getData } from "./Controller/TaskController.js";
 import { getDB } from "./db.js";
 const app = express();
@@ -11,9 +15,11 @@ const connect = getDB();
 //  CRUD -> POST, GET, PUT, DELETE
 // JSON
 
-app.get("/", getData);
+app.get("/:userId", getData);
 
 app.post("/create-task", createTask);
+app.delete("/delete-task/:id", deleteTask);
+app.put("/update-task/:id", updateTask);
 
 app.post("/auth/signup", createUser);
 
